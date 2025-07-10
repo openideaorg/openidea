53afe0eca923f6000a97358df55feab8e817e1b1
Moonstar <33352831+Sony17@users.noreply.github.com>
2025-07-10 14:11:18 +0530

    Add header and footer to placeholder pages

---
+++ app/whitepaper/page.tsx
@@ -0,0 +1,14 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+export default function WhitepaperPage() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow p-8">
+        <p>Coming soon</p>
+      </main>
+      <Footer />
+    </div>
+  );
+}
